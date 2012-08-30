900d23725ae0b1827e3011d511e6f8b09274c73e
root <root@UnderscoreXDevlpt.(none)>
2012-08-30 15:02:41 -0400

    Correcting Prefixed variable inside AptSrcBuildPackage

--- PackageCompiler.sh
+++ PackageCompiler.sh
@@ -33,15 +33,15 @@ function AptSrcBuildPackage()
  local InternalCXX=${ASBPCXX:=/usr/bin/x86_64-linux-gnu-gxx-4.4}
  local InternalLD=${ASBPLD:=/usr/bin/x86_64-linux-gnu-gcc-4.4}
 
- local StartPath=${ASIPPath:=./}
+ local StartPath=${ASBPPath:=./}
 
- local PackageGetter=${ASIPPackageGetter:=GetPackageLadspa} ;
+ local PackageGetter=${ASBPPackageGetter:=GetPackageLadspa} ;
  local ArrayPackage=( $( ${PackageGetter} ) ) ;
  
  for (( intx=0 ; intx <= ${#ArrayPackage[@];intx++}+1 )) ; do 
   local Item=${ArrayPackage[${intx}]} ; 
   cd ${StartPath} ;
-  CC=${InternalCC} CXX=${ASBPCXX} LD=${ASBPLD} apt-src build ${Item} ; 
+  CC=${InternalCC} CXX=${InternalCXX} LD=${ASBPLD} apt-src build ${Item} ; 
  done
 
 }
