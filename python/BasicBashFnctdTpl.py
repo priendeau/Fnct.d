30aaf3202c8fe4df4a584998e4f125faa8fbd43c
Maxiste Deams <maxiste@UnderscoreX-Devlpt.(none)>
2013-02-02 18:27:17 -0500

    Adding important module like iterpipes, sqldict, GenericCache.

--- python/BasicBashFnctdTpl.py
+++ python/BasicBashFnctdTpl.py
@@ -2,13 +2,36 @@
 # -*- coding: utf-8 -*-
 
 ### file BasicBashFnctdTpl.py
+try:
+  ThisSetTest=Set(['1','2','3'])
+except NameError:
+  import sets
+  from sets import Set
+  print "Warning Decorator Error Raiser need some default import like sets and sets.Set.\nTest simply failed. importing manually"
 
+import sys, os, re, cStringIO, cPickle, zipfile, zlib, tempfile, time,  datetime
+import sqlitedict, iterpipes 
+
+
+try:
+  from iterpipes import cmd, bincmd, linecmd, run, call, check_call, cmdformat, compose
+except ImportError:
+  #print "Warning cmdformat not compatible or can not be loaded properly .\nLoading cmd, bincmd, linecmd, run, call, check_call, compose.\n"
+  from iterpipes import cmd, bincmd, linecmd, run, call, check_call, compose
+
+from sqlitedict import SqliteDict
+from optparse import OptionParser  
 from mako.template import Template
+from GenericCache.GenericCache import GenericCache
+from GenericCache.decorators import cached
+
 
 class AttributeGenerationDecor( ):
   DisplayKargs2AttrItemAdded=True
   DictAttrRefferal                = 'DictAttrAdd'
   DictAttrRefferalType            = list()
+  DictHolderDictExpt              = { 'CurrentAttrRaiserName':{ 'property':'AttrRaiser' },
+                                      'CurrentAttrName':{'property':'AttrList'} }
   DictAttrRefferalAdd             = 'append'
   ErrorClassReceivedAttrList      = None
   ErrorClassReceivedAttrListName  = None
@@ -27,14 +50,21 @@ class AttributeGenerationDecor( ):
           for ItemName in kwargs.keys():
             if AttributeGenerationDecor.DisplayKargs2AttrItemAdded == True :
               print "Processing Key %s, Value: %s" % ( ItemName, kwargs[ItemName] )
+            if ItemName in AttributeGenerationDecor.DictHolderDictExpt.keys():
+              ClassTinyDecl.
             setattr( ClassTinyDecl, ItemName , kwargs[ItemName] )
             ListAttr.append( ItemName )
           func( *args ,**kwargs )
         return inner
     return decorator
 
-class ObjectFnctdHolder( object ):
+class BasicObjectWarning( Warning ):
+  def __init__( self , value ):
+    print "Object {}, raise an exception with value {}".format( self.__class__.__name__, value  )
+    Warning.__init__( value )
 
+class ObjectFnctdHolder( object ):
+  
   ListAttr=list( )
 
   CurrentAttr             = None 
@@ -63,8 +93,9 @@ class ObjectFnctdHolder( object ):
     return self.ListAttr
 
   def SetAttrList( self, value  ):
-    if self.AttrRaiser == None:
-      if 
+    if self.AttrRaiser != None:
+      if hasattr( self, value ):
+        raise getattr( self, CurrentAttrRaiser )( value )
 
   def DelAttrList( self ):
     self.ListAttr = list() 
@@ -73,7 +104,22 @@ class ObjectFnctdHolder( object ):
 
 
 class FnctdTemplateModel( object ):
+  cache = GenericCache()
 
+  CurrTemplateName      = None
+  CurrTemplateRendered  = None 
+
+  def GetTemplateName( self ):
+    return self.CurrTemplateName
+
+  def SetTemplateName( self, value ):
+    self.CurrTemplateName  = value
+
+  def DelTemplateName( self ):
+    self.CurrTemplateName = None
+
+  TemplateName = property( GetTemplateName , SetTemplateName, DelTemplateName )
+  
   FnctdFunction = Template( """
   function ${ FunctionName }()
   {
@@ -112,8 +158,12 @@ class FnctdTemplateModel( object ):
     fi
   }""" )
 
+  @cached(cache)
+  def ParseTemplate( self )
+
   @AttributeGenerationDecor.Kargs2Attr( ObjectFnctdHolder )
   def __init__( self, **Kargs ):
     self.FnctdFunction.render( **Kargs ) 
   
-
+if __name__.__eq__( '__main__' ):
+  AInstanceBashParser=( 'BasicObjectWarning'
