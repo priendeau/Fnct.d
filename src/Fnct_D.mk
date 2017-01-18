aaab139e9fa5aeceabd8d5646f45a8c88cdddc91
priendeau <maxistedeams@gmail.com>
2017-01-17 22:00:25 -0500

    Added source_mgmt.mk for makefile of source code. Completed signature process inside the signed_file.mk, target install/uninstall seems to make sense (Makefile) . Linking of bash_lib inside Fnct_D.mk untested, but seems to follow bash logic. Almost the deployed and studyed version.

--- src/Fnct_D.mk
+++ src/Fnct_D.mk
@@ -1,7 +1,10 @@
 
-PATH_INSTALL_LIB				:=/etc/Fnct.D
-
-bash_lib 								= _sub_Structure \
+PATH_INSTALL_LIB				:=/etc/init.d/Fnct.D
+STARTUP_FNCTLIB					:=_sub_Structure fnct_debian_lib
+UPDATE_INIT_BASH				:=bashrc_update
+BASH_INIT								:=.bashrc
+bash_lib 								= md_cd \
+													_sub_Structure \
 													fnct_debian_lib \
 													_sub_Function_Appliance \
 													_sub_jack_script \
@@ -10,20 +13,32 @@ bash_lib 								= _sub_Structure \
 													_sub_ssh_handler \
 													_sub_vlc_services \
 													_sub_binary \
-													_sub_PythonBoost
-
-
+													_sub_PythonBoost \
+													023102e0-1833-11e3-98a2-001b3875b29c \
+													69968204-0cf6-11e3-99bc-001b3875b29c \
+													131cb830-8cd7-11e6-8b1d-001e4c8856d6 \
+													6e063cbc-0ce2-11e3-8879-001b3875b29c \
+													877d7e9a-1001-11e3-98a2-001b3875b29c \
+													aa49dbce-3079-11e3-98a2-001b3875b29c \
+													50ccda66-2ebb-11e3-98a2-001b3875b29c \
+													6403c722-28a6-11e3-98a2-001b3875b29c \
+													950966fa-0e67-11e3-98a2-001b3875b29c \
+													0f86b448-72f4-11e6-8b1d-001e4c8856d6 
 
 define MakePathInstall
-@if [ ! -d $(PATH_INSTALL_LIB) ] ; then sudo -s mkdir $(PATH_INSTALL_LIB) ; fi 
+@if [ ! -d $(PATH_INSTALL_LIB) ] ; then $(SUDO) $(SUDO_FLAGS) /bin/mkdir $(PATH_INSTALL_LIB) ; fi 
 endef
 
-#Pragma function of LinkLib
+#Pragma Variable as function of LinkLib
 define LinkLib
-ln -sf ../$(%D)$(1) $(PATH_INSTALL_LIB)/$(1)
+@if [ ! -l $(PATH_INSTALL_LIB)/$(1) -o ! -f $(PATH_INSTALL_LIB)/$(1) ] ; then \
+  ln -sf ../$(%D)$(1) $(PATH_INSTALL_LIB)/$(1) ; \
+else \
+	$(ECHO) "Link or File $(PATH_INSTALL_LIB)/$(1) already point to your development." ; \
+fi ; 
 endef
 
-#Pragma function of UnProtectFile
+#Pragma Variable as function of UnProtectFile
 define StartLink
 $(foreach StrFile,$(1),$(call LinkLib,$(StrFile)))
 endef
@@ -31,5 +46,5 @@ endef
 path-install:
 	$(MakePathInstall)
 
-fnct.d:	
+fnct.d:	path-install
 	$(call StartLink,$(bash_lib))
--- src/signed_file.mk
+++ src/signed_file.mk
@@ -8,8 +8,6 @@ FAILED_MSG							:=FAILED
 SIGNED_EXTENSION				:=.signed
 
 ### Related to FILE_SIGNATURE 
-
-
 APPLY_SIGNATURE					:=$(SIGNATURE) enc -e
 REVERT_SIGNATURE				:=$(SIGNATURE) enc -d
 
@@ -51,8 +49,14 @@ $$( $(call BASE64_TO_SIG,$(1)) )
 endef
 
 #Pragma function of SHA1SUM_CERT_PROC
+#ill to beleive or not some text-editor
+#not following the starting line 
+#$$( StrCheck=$$($(call SHA1SUM_CERT_SIG,$(1) ) );\
+#... to the end before endef 'fi ; )' and to 
+#send a eyes warning to brace problems, while 
+# make did not notice it. 
 define SHA1SUM_CERT_PROC
-$$( StrCheck=$$($(call SHA1SUM_CERT_SIG,$(1))) ;\
+$$( StrCheck=$$($(call SHA1SUM_CERT_SIG,$(1) ) );\
 if [ "$${StrCheck}" == "$(2)" ] ; then \
 	$(ECHO) OK ; \
 else \
@@ -60,32 +64,22 @@ else \
 fi ; )
 endef
 
-#Pragma function of ExitWithStateMsg ISSHA1SUM_CERT, FAILED, SIGN_ERR_BAD_DGST, 1 
-# Param1 : String Variable to compare
-# Param2 : String Variable to compare with Param1
-# Param3 : Variable name, the Error Message
-# Param4 : Exit State (usually 1)
-#
-define ExitWithStateMsg
-@if [ "$(1)" == "$(2)" ] ; then \
-	$(ECHO) $(3);\
-	exit $(4) ; \
-fi 
-endef
-
 GET_DECODED_SIGNATURE		:=$(call GET_SIGNATURE,$(FILE_SIGNATURE))
 ISSHA1SUM_CERT					:=$(call SHA1SUM_CERT_PROC,$(FILE_SIGNATURE),$(FILE_SIG_SHA1SUM))
 
 #Pragma function of SHA1SUM_CERT_PROC
 #$(ECHO) "Applying signature for $(1), signed file: $(1))$(SIGNED_EXTENSION)"
+# removed from action in signing appliation, NASM using .asm and GAS unsing .S 
+# in extension format, overwrite file like shell-bash.asm / shell-bash.S 
 define ApplySignature
 $(ECHO) "Applying signature for file $(1)" ; \
-$(APPLY_SIGNATURE) $(SIGNATURE_TYPE) -a -k $(GET_DECODED_SIGNATURE) -in $(1) -out $(basename $(1))$(SIGNED_EXTENSION) ; 
+$(APPLY_SIGNATURE) $(SIGNATURE_TYPE) -a -k $(GET_DECODED_SIGNATURE) -in $(1) -out $(1)$(SIGNED_EXTENSION) ; 
 endef
 
 #Pragma function of RevertSignature
 define RevertSignature
-$(REVERT_SIGNATURE) $(SIGNATURE_TYPE) -a -k $(GET_DECODED_SIGNATURE) -in $(basename $(1))$(SIGNED_EXTENSION) -out $(1) ; 
+$(ECHO) "Reverting signature for file $(1)$(SIGNED_EXTENSION)" ; \
+$(REVERT_SIGNATURE) $(SIGNATURE_TYPE) -a -k $(GET_DECODED_SIGNATURE) -in $(1)$(SIGNED_EXTENSION) -out $(1) ; 
 endef
 
 #Pragma function of ProtectFile
@@ -109,7 +103,50 @@ check_signature: digest
 	@$(ECHO) "ISSHA1SUM_CERT        : $(ISSHA1SUM_CERT)"
 
 protect:	check_signature
-	$(call ProtectFile,$(protected))
+	@$(call ProtectFile,$(protected))
 
 unprotect:	check_signature
-	$(call UnProtectFile,$(protected))
+	@$(call UnProtectFile,$(protected))
+
+### calling this target it should check
+### for corresponding file name .S and 
+### apply a signature. If this one is 
+### not member of protected files you
+### should apply yourself the revertion
+### when you want to read out the file.
+### call make FILE.EXT.revert explain
+### some targets ahead. 
+%.S.signed:
+	@$(call ProtectFile,$(basename $@))
+	
+%.asm.signed:
+	@$(call ProtectFile,$(basename $@))
+
+### This one just can sign about everything
+### else whishing to sign-it.
+%.%.signed:
+	@$(call ProtectFile,$(basename $@))
+
+### This extension is not existing but we 
+### can use it to call a single signature
+### revertion of a Gnu GAS signed file.
+### Target %S / %asm are already taken
+### for corresponding C code conversion.
+###  
+%.S.revert:
+	@$(call UnProtectFile,$(basename $@))
+	
+### This extension is not existing but we 
+### can use it to call a single signature
+### revertion of a NASM  signed file.
+### Target %S / %asm are already taken
+### for corresponding C code conversion.
+%.asm.revert:
+	@$(call UnProtectFile,$(basename $@))
+
+
+### This one just can revert signature of 
+### about everything else whishing to revert
+### a signature.
+%.%.revert:
+	@$(call UnProtectFile,$(basename $@))
