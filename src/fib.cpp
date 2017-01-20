8c524bfa842eb883fb409811989a853fba3f9ccf
priendeau <maxistedeams@gmail.com>
2017-01-19 21:28:01 -0500

    Updating Makefile. Adding NewPathModel.tar.bz2, as template deployment for Md_Cd example. Also added fib.cpp used to generate Fibonnacci number out of following function added inside _sub_binary ( GetFibLevel, GetHighestFibHarmonic, GetValueScaledFibHarmonic, FibBinaryLevelRepr), which are tool that generate Fibonacci number and from decomposition of number from Fibonnacci result, it build a position relative binary representation of a number from Fibonnacci decomposition to use MakeHuffmanPair, to search grouped position of packed position of Nth-fibonnacci level into a huffman tree and play with huffman packing-binary to test if this algorithm is working. Use of relative packed postion into binary form is not a logarithmic scale form for having not equivalent by scaling the value, but using Fibonnacci Level as incremental value.  Some may also know Fibonnacci also work in limit and a lower level, adding number making this next fibonnacci level can be higher as this level.

---
+++ src/fib.cpp
@@ -0,0 +1,50 @@
+#ifndef __FIB_CPP__
+#define __FIB_CPP__
+
+
+// This program calculates the nth fibonacci number
+// using alrogirhtm 1A: naive binary recursion
+//
+// compiled: g++ -std=c++0x -Wall -Wextra -pedantic -O2 -march=native -o f1a f1a.cc
+// executed: ./f1a n
+//
+
+#include "fib.h"
+
+using namespace std; 
+
+// Naive binary recursion: F(n) = F(n-1) + F(n-2)
+long fib(int n) 
+{
+  return n<2 ? n : fib(n-1) + fib(n-2);
+}
+// Function f(n) handles the negative arguments: F(-n) = F(n)*(-1)^(n+1) 
+long f(int n) 
+{
+ if(n<0)
+    return n%2 ? fib(-n) : -fib(-n);
+ else
+    return fib(n);
+}
+// Function f_print prints out n'th Fibonacci number
+void fib_print(int n) 
+{
+ cout << n << "th Fibonacci number is " << f(n) << '\n';
+}
+// entry point:
+// convert the first argument to a number and calls fib_print
+// if argument did not exist or wasn't a number, show usage information
+int main(int argc, char** argv) 
+{
+ vector< string > args(argv, argv+argc);
+ try 
+ {
+  fib_print(stoi(args.at(1)));
+ } catch ( exception& ) 
+   {
+     cout << "Usage: " << args[0] << " <n>\n";
+   }
+}
+
+
+#endif // __FIB_CPP__
