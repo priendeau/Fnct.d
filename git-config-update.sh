fd7063e9abd8c5f557767b9219b36929a04d624b
priendeau <maxistedeams@gmail.com>
2017-04-01 15:12:05 -0400

    Added git-config-update.sh, github configuration for safe development.

---
+++ git-config-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+git config core.filemode true
+git config core.bare false
+git config core.logallrefupdates true
+git config core.protectntfs true
+git config core.protecthfs true
+git config core.ignorecase false
+git config core.hidedotfiles false
+git config core.autocrlf true
+git config core.symlinks true
